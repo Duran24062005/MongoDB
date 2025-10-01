668be70b4264eb6654cad499bf20523c2a673860
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-10-01 17:17:59 -0500

    feat: :fire: new information was added

--- campus/incautaciones_ma_20250930/playground.js
+++ campus/incautaciones_ma_20250930/playground.js
@@ -1,46 +1,8 @@
-// Collections to Incautaciones de MA 20250930
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 
-use ('Incautacionesde_MA_DB_20250930');
-
-
-db.Departamentos.insertOne(
-    {
-        id_departamento: 1,
-        nombre_departamento: 'ANTIOQUIA',
-        codigo_departamento: 5
-    }
-);
-
-
-db.Municipios.insertOne(
-    {
-        id_municipio: 1,
-        nombre_municipio: 'CASERES',
-        codigo_municipio: 5120,
-        departamento_id: 1,
-    }
-);
-
-db.Hechos.insertOne(
-    {
-        id_hecho: 1,
-        fecha_hecho: '01-01-2010',
-        cantidad_incautada: 67,
-        municipio_id: 1,
-        unidad_medida_id: 1
-    }
-);
-
-
-db.UnidadMedida.insertOne(
-    {
-        id_unidad_medida: 1,
-        nidad_medida: 'KILOGRAMO',
-        siglas_unidad_medida: 'KG',
-    }
-);
-
-
+// The current database to use.
+use("Incautacionesde_MA_DB_20250930");
 
 // Supongamos que ya tienes tu colección Incautaciones con los campos:
 // { "FECHA HECHO": "...", "MUNICIPIO": "...", "CANTIDAD": "...", "UNIDAD": "..." }
--- campus/incautaciones_ma_20250930/script.js
+++ campus/incautaciones_ma_20250930/script.js
@@ -1,49 +1,7 @@
-// Collections to Incautaciones de MA 20250930
-
-use ('Incautacionesde_MA_DB_20250930');
-
-
-db.Departamentos.insertOne(
-    {
-        id_departamento: 1,
-        nombre_departamento: 'ANTIOQUIA',
-        codigo_departamento: 5
-    }
-);
-
-
-db.Municipios.insertOne(
-    {
-        id_municipio: 1,
-        nombre_municipio: 'CASERES',
-        codigo_municipio: 5120,
-        departamento_id: 1,
-    }
-);
-
-db.Hechos.insertOne(
-    {
-        id_hecho: 1,
-        fecha_hecho: '01-01-2010',
-        cantidad_incautada: 67,
-        municipio_id: 1,
-        unidad_medida_id: 1
-    }
-);
-
-
-db.UnidadMedida.insertOne(
-    {
-        id_unidad_medida: 1,
-        nidad_medida: 'KILOGRAMO',
-        siglas_unidad_medida: 'KG',
-    }
-);
-
-
-
 // Supongamos que ya tienes tu colección Incautaciones con los campos:
 // { "FECHA HECHO": "...", "MUNICIPIO": "...", "CANTIDAD": "...", "UNIDAD": "..." }
+
+use("Incautacionesde_MA_DB_20250930");
 
 // Contadores para IDs
 let idHecho = 1;
